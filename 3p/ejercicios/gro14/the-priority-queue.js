e5ca65c160d917a99734c86c57e99cd47481c9cd
CHEGALO <62453209+CHEGALO@users.noreply.github.com>
2020-06-12 13:53:22 -0500

    Add files via upload

---
+++ 3p/ejercicios/gro14/the-priority-queue.js
@@ -0,0 +1,34 @@
+
+function PriorityQueue() {
+    let items = [];
+
+    function QueueElement (element, priority){ // {1}
+
+    this.element = element;
+    this.priority = priority;
+   }
+
+    this.enqueue = function(element, priority){
+     let queueElement = new QueueElement(element, priority);
+    }
+    let added = false;
+
+    for (let i=0; i<items.length; i++){
+        if (queueElement.priority < items[i].priority){ // {2}
+        items.splice(i,0,queueElement); added = true;
+        break; // {4}
+        } 
+    }
+
+    if (!added){ 
+        items.push(queueElement); //{5}
+    } 
+};
+
+this.print = function(){
+    for (let i=0; i<items.length; i++){
+        console.log(`${items[i].element} -
+    ${items[i].priority}`); 
+    }
+};
+//other methods - same as default Queue implementation }
