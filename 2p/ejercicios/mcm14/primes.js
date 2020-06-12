71d9c90ad6dc794b43f29f876ba36d86efe55d94
CHEGALO <62453209+CHEGALO@users.noreply.github.com>
2020-06-12 13:50:20 -0500

    Add files via upload

---
+++ 2p/ejercicios/mcm14/primes.js
@@ -0,0 +1,12 @@
+let arr = [2,-4]
+
+   function swap(i,k){
+     t=arr[i]=arr[K]
+     arr[i]=arr[k]
+     arr[k] = t
+ 
+   
+}
+
+swap(0,1)
+console.log(arr)
---
+++ 2p/ejercicios/mcm14/radian2degree.js
@@ -0,0 +1,10 @@
+function radian2degree(radian){
+
+
+return radian*(180/Math.PI);
+
+}
+
+
+console.log(radian2degree(Math.PI));
+console.log(radian2degree(2Math.PI));
